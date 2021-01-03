9218879976aa6c973393ee0a348fc0b40973f39e
Eber <doneber98@gmail.com>
2021-01-02 20:29:42 -0400

    Multiple entry files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
 
 module.exports = {
-	entry: path.resolve(__dirname, 'index.js'),
+	entry: {
+		home: path.resolve(__dirname, 'src','js','index.js'),
+		greetings: path.resolve(__dirname, 'src','js','greetings.js'),
+	},
 	mode: "development",
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'js/[name].js'
 	}
 }
