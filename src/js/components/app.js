af09063e6c2865d0c2e467706e2d2173048487e2
Eber <doneber98@gmail.com>
2021-01-04 18:14:29 -0400

    Jugando con React

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../data.json'
+import Loader from './loader'
 
 function App() {
+	const [loaderList, setLoaderList] = useState([])
+	function handleClick() {
+		setLoaderList(data.loaders)
+	}
 	return (
 		<div>
-			Soy el App de react
+			<ul>
+				{
+					loaderList.map(item => <Loader {...item} key={item.id} />)
+				}
+			</ul>
+			<button onClick={handleClick}> Mostrar lo aprendido</button>
 		</div>
 	)
 }
